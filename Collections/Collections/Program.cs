5e71cbda3b9b27533ceebf0d58138450f996976c
majuliah <majulialcarvalho@gmail.com>
2026-08-22 16:35:48 -0300

    collections: arrays

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,41 @@
+﻿// arrays are a collection of variables of the same type.
+// They are fixed in size and can be multidimensional.
+// The elements of an array are accessed using an index, which starts at 0.
+// Arrays can be declared and initialized in various ways.
+
+// here is how we declare an array
+int[] numbers = new int[3]; // declares an array of 3 integers
+
+// here is how we set values in an array
+numbers[0] = 1;
+numbers[1] = 2;
+numbers[2] = 3;
+
+// here is how we get values from an array
+int firstNumber = numbers[0]; // gets the first element of the array
+int secondNumber = numbers[1];
+
+// here is how we declare and initialize an array 
+int[] newNumbers = new int[]
+{
+    5,
+    6,
+    7,
+    8
+};
+
+int[] numbers3 =
+{
+    10,
+    11,
+    12
+};
+
+int[] numbers4 = [3, 4, 5];
+
+// here is how we get the length of an array
+int length = numbers3.Length;
+
+
+
+
