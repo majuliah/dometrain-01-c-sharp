5b5f1c16a4efa37b6adfcf9414ee322b57f6bf6b
majuliah <majulialcarvalho@gmail.com>
2026-08-22 15:24:18 -0300

    control flow: switch statements and expressions

--- ControlFlow/ControlFlow/Program.cs
+++ ControlFlow/ControlFlow/Program.cs
@@ -1,27 +1,4 @@
-﻿// ternary operators are used to assign a
-// value to a variable based on a condition.
-// The syntax is: (condition) ? value_if_true : value_if_false;
-
-int x = 10;
-string result = (x > 5) 
-    ? "x is greater than 5" 
-    : "x is less than or equal to 5";
-Console.WriteLine(result);
-
-result = x == 10 
-    ? "x is equal to 10" 
-    : "x is not equal to 10";
-Console.WriteLine(result);
-
-result = x < 20
-    ? "x is less than 20"
-    : "x is greater than or equal to 20";
-Console.WriteLine(result);
-
-
-
-
-#region 0.1 if statements
+﻿#region 0.1 if statements
 // the most basic type of control flow is the if statement,
 // which allows us to execute a block of code only if a certain
 // condition is true. If the condition is false, the block of code is skipped.
@@ -126,4 +103,78 @@ else
     Console.WriteLine("The number is between 1 and 5");
 }
 
+#endregion
+#region 0.2 Ternary Operators
+// ternary operators are used to assign a
+// value to a variable based on a condition.
+// The syntax is: (condition) ? value_if_true : value_if_false;
+
+int x = 10;
+string result = (x > 5)
+    ? "x is greater than 5"
+    : "x is less than or equal to 5";
+Console.WriteLine(result);
+
+result = x == 10
+    ? "x is equal to 10"
+    : "x is not equal to 10";
+Console.WriteLine(result);
+
+result = x < 20
+    ? "x is less than 20"
+    : "x is greater than or equal to 20";
+Console.WriteLine(result);
+#endregion
+# region 0.3 switch statements and expressions
+// The switch statement is used to select 
+// one of many code blocks to be executed.
+// The switch expression is used to select
+// many code blocks to be executed
+
+// here is an example of a switch statement
+int dayOfWeek = 4;
+switch (dayOfWeek)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Invalid day of the week");
+        break;
+}
+Console.WriteLine("This is after the switch statement: ");
+
+//here is an example of a switch expression
+string dayOfWeekName = "Thursday";
+string result = dayOfWeekName switch
+{
+    "Monday" => "First day of the week",
+    "Tuesday" => "Second day of the week",
+    "Wednesday" => "Third day of the week",
+    "Thursday" => "Fourth day of the week",
+    "Friday" => "Fifth day of the week",
+    "Saturday" => "Sixth day of the week",
+    "Sunday" => "Seventh day of the week ",
+    _ => "Invalid day of the week"
+};
+
+Console.WriteLine(result);
 #endregion
