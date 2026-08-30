78c9152159d1885935b2dae359b782af6b6ee367
majuliah <majulialcarvalho@gmail.com>
2026-08-30 13:32:04 -0300

    Loops: if

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,4 +1,56 @@
-﻿// While loops and do while loops are used
+﻿#region For Loops
+// a for loop is a loop that runs a specific number of times
+// we saw how to count with while loops...
+// but a for loop is designed to count!
+
+// here is the syntax for a for loop:
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+// a for loop has three parts:
+// 1. the initializer: int = 0;
+// 2. the condition: i < 10;
+// 3. the iterator: i++
+
+// note that we can't access i outside of the loop!
+// i = 123; // this will not work!
+
+// we can use break and continue in a for loop as well,
+// just like we did with while loop
+
+// here's an example of a for loop with a break:
+for (int i = 0; i <10; i++)
+{
+    if (i == 5)
+    {
+        Console.WriteLine("We're outta here!");
+        break; //break, breakes the loop
+    }
+    Console.WriteLine(i);
+}
+
+// here's an exemple of a for loop with continue
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+    {
+        Console.WriteLine("Skipping the 5!");
+        continue; // continue will go back to the next iteration
+    }
+    Console.WriteLine(i);
+}
+
+
+
+#endregion
+
+
+
+# region 1: Do and DoWhile
+
+// While loops and do while loops are used
 // to execute a block of code repeatedly.
 
 //here is what a while loop looks like
@@ -57,7 +109,7 @@ while (count < 5)
         break;
     }
 }
-
+#endregion
 
 
 
