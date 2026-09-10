a7827b8ace6e46fbdfc3cc96969f96cb5bf36756
majuliah <majulialcarvalho@gmail.com>
2026-09-10 00:18:30 -0300

    Debugging and Exceptions

---
+++ DebuggingAndExceptions/Program.cs
@@ -0,0 +1,14 @@
+﻿// debugging is the process of finding and 
+// fixing errors within a computer program.
+// errors in c# programs are called exceptions
+// exceptions are thrown when an error occurs and can be caught using try-catch blocks
+
+// lets create a simple program that throws an exception
+
+int IntegerDivision(int numerator, int denominator)
+{
+    return numerator / denominator;
+}
+
+// the program will throw an exception when we try to divide by zero
+int result = IntegerDivision(10, 0);
