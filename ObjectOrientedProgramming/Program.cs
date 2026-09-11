ce99c2db4de335aca6a1d9ed811b86727d859526
majuliah <majulialcarvalho@gmail.com>
2026-09-11 18:20:41 -0300

    Object oriented Programming: Instance and Static

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -148,8 +148,8 @@ class Person4
         return this._name;
     }
 }
-Person4 nomeNovoPessoa = new Person4();
-Console.WriteLine(nomeNovoPessoa.GetName());
+//Person4 nomeNovoPessoa = new Person4();
+//Console.WriteLine(nomeNovoPessoa.GetName());
 
 // there are other access modifiers that we wont cover in this course
 
@@ -172,16 +172,91 @@ class Person5
         set { _name = value; } // this is the setter
     } 
 }
-Person5 johnWithProperty = new Person5();
-Console.WriteLine(johnWithProperty.Name);
-Console.WriteLine(johnWithProperty.Name2);
-Console.WriteLine(johnWithProperty.Name3);
+////Person5 johnWithProperty = new Person5();
+//Console.WriteLine(johnWithProperty.Name);
+//Console.WriteLine(johnWithProperty.Name2);
+//Console.WriteLine(johnWithProperty.Name3);
 
-Console.WriteLine("Setting the name...");
-johnWithProperty.MutableName = "John Doe";
-Console.WriteLine(johnWithProperty.MutableName);
+//Console.WriteLine("Setting the name...");
+//johnWithProperty.MutableName = "John Doe";
+//Console.WriteLine(johnWithProperty.MutableName);
 
-Console.WriteLine(johnWithProperty.MutableName);
+//Console.WriteLine(johnWithProperty.MutableName);
+
+
+#endregion
+
+#region Static vs Instances
+
+public sealed class StaticVsInstance
+{
+    public void RunExample()
+    {
+        // static is a modifier that makes a member belong to a type, rather than an "instance" of a type
+        // we can even make entire classes static.
+
+        // note that we cannot make an instance of "MyStaticClass" because it is marked static
+        // MyStaticClass myStaticClass = new MyStaticClass(); // this will not work!
+
+        // static classes cannot have instance members, so anything inside of a static class must also be static
+        MyStaticClass.MyStaticMethod();
+
+        // notice how we don't need to make an instance of MyStaticClass to call MyStaticMethod?
+        // this is just like Console.WriteLine! This means that Console.WriteLine is a static method!
+
+        // on non-static classes, we *can* add static members
+        MyNonStaticClass myNonStaticClass1 = new MyNonStaticClass();
+        MyNonStaticClass myNonStaticClass2 = new MyNonStaticClass();
+
+        Console.WriteLine("Before mutating properties on MyNonStaticClass...");
+        myNonStaticClass1.MyInstanceMethod();
+        myNonStaticClass2.MyInstanceMethod();
+        MyNonStaticClass.MyStaticMethod();
+
+        // let's mutate these things and see what happens!
+        myNonStaticClass1.MyInstanceProperty = "Dev";
+        myNonStaticClass2.MyInstanceProperty = "Leader";
+        MyNonStaticClass.MyStaticProperty = "Nick Cosentino";
+
+        Console.WriteLine("After mutating properties on MyNonStaticClass...");
+        myNonStaticClass1.MyInstanceMethod();
+        myNonStaticClass2.MyInstanceMethod();
+        MyNonStaticClass.MyStaticMethod();
+
+        // static members are accessible across all instances. this can be
+        // convenient, but can also lead to bugs if you're not careful!
+        // personally - I advise a lot of caution with using static.
+    }
+
+    static class MyStaticClass
+    {
+        public static void MyStaticMethod()
+        {
+            Console.WriteLine("I am a static method!");
+        }
+    }
+
+    class MyNonStaticClass
+    {
+        public string MyInstanceProperty { get; set; }
+
+        public static string MyStaticProperty { get; set; }
+
+        public static void MyStaticMethod()
+        {
+            Console.WriteLine($"The static property value is: {MyStaticProperty}");
+
+            // this will not work because MyInstanceProperty is not static!
+            //Console.WriteLine($"The instance property value is: {MyInstanceProperty}");
+        }
+
+        public void MyInstanceMethod()
+        {
+            Console.WriteLine($"The static property value is: {MyStaticProperty}");
+            Console.WriteLine($"The instance property value is: {MyInstanceProperty}");
+        }
+    }
+}
 
 
 #endregion
